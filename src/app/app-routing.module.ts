43b59f61c3f29ef235d3c99743c2f6915167c7e0
LuisCutzal <70243302+LuisCutzal@users.noreply.github.com>
2021-04-11 17:54:12 -0600

    RouterModule - Rutas en nuestra aplicación

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule} from '@angular/core';
+import { Routes, RouterModule } from "@angular/router";
+import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
+import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
+import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
+
+const routes:Routes=[
+    {
+        path:"",//primer componente que se mostraria
+        component:PorPaisComponent,
+        pathMatch:"full"
+    },
+    {
+        path:"Region",
+        component:PorRegionComponent
+    },
+    {
+        path:"Capital",
+        component:PorCapitalComponent
+    },
+    {
+        path:"pais/:id",
+        component:VerPaisComponent
+    },
+    //excepcion
+    {
+        path:"**",
+        redirectTo:"" 
+    }
+];
+
+@NgModule({
+    imports:[
+        RouterModule.forRoot(routes)
+    ],
+    exports:[
+        RouterModule
+    ]
+}) 
+export class AppRotingModule{
+
+}
