fe1dc2b93bb7f304bfa3fc58a091ebf9afb70920
LuisCutzal <70243302+LuisCutzal@users.noreply.github.com>
2021-04-11 20:46:52 -0600

    Llenar la tabla de países

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Contry } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -11,18 +12,21 @@ export class PorPaisComponent {
   
   termino:string="";
   existeError:boolean=false;
-  
+  paises:Contry[]=[];
+
   constructor(private paisService:PaisService) { }
 
   buscar(){
     this.existeError=false;
     console.log(this.termino);
+    
     this.paisService.buscarPais(this.termino)
-    .subscribe((paises)=>{
-      console.log(paises);
+    .subscribe((argumento)=>{
+      console.log(argumento);
+      this.paises=argumento;
     },(err)=>{
       this.existeError=true;
-
+      this.paises=[];
     });
   }
 }
