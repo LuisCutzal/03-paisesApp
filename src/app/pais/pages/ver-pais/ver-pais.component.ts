7f383aee2a5f0e1a54fd144f666fca77fe7e306b
LuisCutzal <70243302+LuisCutzal@users.noreply.github.com>
2021-04-16 18:33:18 -0600

    RxJs - SwitchMap

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from "rxjs/operators"; //te permite resivir un observable y regresar un observable
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -16,18 +17,34 @@ export class VerPaisComponent implements OnInit {
     ) { }//para suscribirse
 
   ngOnInit(): void { //un buen lugar para suscribirse, es cuando el componente esta inicializado
+    /*
     this.activateRoute.params
     .subscribe(params=>{
       console.log(params);
-      /*se puede colocar de la forma anterior el params o colocarlo como
-      .subscribe(({id}))=>{
-        console.log(id);
-      } */
+      //se puede colocar de la forma anterior el params o colocarlo como DESESTRUCTURACION DE ARGUMENTOS
+      //.subscribe(({id}))=>{
+      //  console.log(id);
+      //}
       this.PaisService.buscarIdPais(params.id)//es necesario el codigo del params para que este segundo subscribe funcione
       .subscribe(pais=>{
         console.log(pais);
       });
     });
+     */
+
+    //usando operadores de RxJ
+    this.activateRoute.params
+    .pipe(//especificar cualquier cantidad de operadores que trabajaran con el producto del observable: this.activateRoute.params
+      switchMap((para)=>this.PaisService.buscarPais(para.id)) //retorna un observable, se hizo return implicito
+//resive el valor del observable anterior y retorna un nuevo observable, ademas se puede usar la desestructuracion de argumentos, como se ve en el codigo anterior
+    )
+    .subscribe(respuesta=>{
+      console.log(respuesta);
+    });
+
+
+
+
   }
 
 }
