66ff1534eea0afd4d350dde2cf31655de9d6f2fb
LuisCutzal <2842258270404@ingenieria.usac.edu.gt>
2021-04-23 14:43:32 -0600

    ngClass, class y [class.] y Clases de CSS condicionales

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
+  styles: [`
+    button{
+      margin-right:5px;
+    }
+  
+  `
   ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
 
-  constructor() { }
+  regiones:string[]=["africa", "americas", "asia", "europe", "oceania"];
+  regionActiva:string="";
 
-  ngOnInit(): void {
+  activarRegion(region:string){
+    this.regionActiva=region;
+
+  }
+
+  getClaseCss(region:string):string{
+    return (region==this.regionActiva) ? 'btn btn-primary': 'btn btn-outline-primary';
+    //si la region es igual a la region activa regresamos 'btn btn-primary' y si no regresamos 'btn btn-outline-primary'
   }
 
 }
